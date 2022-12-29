97f05eaeda2c831865ef1b2394c4944a5a0ffe33
Sejin Lee <lsj8706@naver.com>
2022-12-29 16:40:39 +0900

    [Setting] #1 - Xcode 프로젝트 생성 및 iOS 버전 14.0으로 설정

---
+++ Runnect-iOS/Runnect-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Runnect-iOS
+//
+//  Created by sejin on 2022/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
