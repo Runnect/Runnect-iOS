439324cacd55651616ba2982d66cba2d35e973f5
Sejin Lee <lsj8706@naver.com>
2023-01-02 02:30:24 +0900

    [Feat] #6 - CustomButton 구현

---
+++ Runnect-iOS/Runnect-iOS/Global/UIComponents/CustomButton.swift
@@ -0,0 +1,72 @@
+//
+//  CustomButton.swift
+//  Runnect-iOS
+//
+//  Created by sejin on 2023/01/02.
+//
+
+import UIKit
+
+public class CustomButton: UIButton {
+    
+    // MARK: - Initialize
+
+    public init(title: String) {
+        super.init(frame: .zero)
+        self.setUI(title)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Methods
+
+extension CustomButton {
+    /// 버튼의 enable 여부 설정
+    @discardableResult
+    public func setEnabled(_ isEnabled: Bool) -> Self {
+        self.isEnabled = isEnabled
+        return self
+    }
+    
+    /// 버튼의 Title 변경
+    @discardableResult
+    public func changeTitle(attributedString: NSAttributedString) -> Self {
+        self.setAttributedTitle(attributedString, for: .normal)
+        return self
+    }
+    
+    /// 버튼의 backgroundColor, textColor 변경
+    @discardableResult
+    public func setColor(bgColor: UIColor, disableColor: UIColor, _ textColor: UIColor = .white) -> Self {
+        self.setBackgroundColor(bgColor, for: .normal)
+        self.setBackgroundColor(disableColor, for: .disabled)
+        self.setAttributedTitle(
+            NSAttributedString(
+                string: self.titleLabel?.text ?? "",
+                attributes: [.font: UIFont.h2, .foregroundColor: textColor]),
+            for: .normal)
+        
+        return self
+    }
+}
+
+// MARK: - UI & Layout
+
+extension CustomButton {
+    private func setUI(_ title: String) {
+        self.layer.cornerRadius = 10
+        
+        self.setBackgroundColor(.m1, for: .normal)
+        self.setBackgroundColor(.g2, for: .disabled)
+        self.setAttributedTitle(
+            NSAttributedString(
+                string: title,
+                attributes: [.font: UIFont.h5, .foregroundColor: UIColor.white]
+            ),
+            for: .normal
+        )
+    }
+}
