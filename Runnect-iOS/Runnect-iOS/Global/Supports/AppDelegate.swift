1e190cf61bc17200583eb6600a919d9817b732e9
Sejin Lee <lsj8706@naver.com>
2023-01-01 15:31:59 +0900

    [Setting] #6 - 라이트 모드, 세로 모드 고정 설정

--- Runnect-iOS/Runnect-iOS/Global/Supports/AppDelegate.swift
+++ Runnect-iOS/Runnect-iOS/Global/Supports/AppDelegate.swift
@@ -10,7 +10,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        // 세로방향 고정
+        return UIInterfaceOrientationMask.portrait
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -30,7 +33,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
