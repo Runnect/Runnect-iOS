94f140b09641e9c49aec731758200cad5ab7ab59
Sejin Lee <lsj8706@naver.com>
2023-01-02 02:22:47 +0900

    [Feat] #6 - CustomNavigationBar 구현

--- Runnect-iOS/Runnect-iOS/Presentation/CourseDrawing/VC/CourseDrawingHomeVC.swift
+++ Runnect-iOS/Runnect-iOS/Presentation/CourseDrawing/VC/CourseDrawingHomeVC.swift
@@ -7,10 +7,24 @@
 
 import UIKit
 
-final class CourseDrawingHomeVC: UIViewController {
-
+final class CourseDrawingHomeVC: UIViewController, CustomNavigationBarDelegate {
+//    lazy var naviBar = CustomNavigationBar(self, type: .title).setTitle("보관함")
+//     lazy var naviBar = CustomNavigationBar(self, type: .titleWithLeftButton)
+//    lazy var naviBar = CustomNavigationBar(self, type: .titleWithLeftButton).setTitle("목표 보상")
+    lazy var naviBar = CustomNavigationBar(self, type: .search).showKeyboard().setTextFieldPlaceholder(placeholder: "출발지 검색")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        naviBar.delegate = self
+        view.addSubviews(naviBar)
+        naviBar.snp.makeConstraints { make in
+            make.leading.top.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(48)
+        }
+    }
+    
+    func searchButtonDidTap(text: String) {
+        print(text)
     }
 }
