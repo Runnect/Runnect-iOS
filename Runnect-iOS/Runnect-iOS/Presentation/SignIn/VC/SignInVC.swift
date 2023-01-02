2ec2220f1d18da48d037e6d4669ffa3792132a52
Sejin Lee <lsj8706@naver.com>
2023-01-02 14:04:21 +0900

    [Feat] #10 - SplashVC UI 구현

---
+++ Runnect-iOS/Runnect-iOS/Presentation/SignIn/VC/SignInVC.swift
@@ -0,0 +1,18 @@
+//
+//  SignInVC.swift
+//  Runnect-iOS
+//
+//  Created by sejin on 2023/01/02.
+//
+
+import UIKit
+
+final class SignInVC: UIViewController {
+
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
