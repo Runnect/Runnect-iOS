83f9d876dc9ac392fc59ac5fc7aaa44c229c2af5
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-01-02 18:09:30 +0900

    [Feat] #15 - NavigationBar 생성

--- Runnect-iOS/Runnect-iOS/Presentation/MyPage/VC/InfoVC/GoalRewardInfoVC.swift
+++ Runnect-iOS/Runnect-iOS/Presentation/MyPage/VC/InfoVC/GoalRewardInfoVC.swift
@@ -6,17 +6,38 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 final class GoalRewardInfoVC: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private lazy var navibar = CustomNavigationBar(self, type: .titleWithLeftButton).setTitle("목표 보상")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationBar()
         setUI()
         // Do any additional setup after loading the view.
     }
 }
 
+// MARK: - Methods
+// MARK: - @objc Function
+
+// MARK: - UI & Layout
+
 extension GoalRewardInfoVC {
+    private func setNavigationBar() {
+        view.addSubview(navibar)
+        
+        navibar.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(48)
+        }
+    }
+    
     private func setUI() {
         view.backgroundColor = .w1
     }
