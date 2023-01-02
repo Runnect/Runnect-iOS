b0697c8bf5e5c3c5b0ee5a9216a9d9d6ef6be6af
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-01-02 18:31:50 +0900

    [Feat] #15 - stamp 이미지와 텍스트 라벨 추가

--- Runnect-iOS/Runnect-iOS/Presentation/MyPage/VC/InfoVC/GoalRewardInfoVC.swift
+++ Runnect-iOS/Runnect-iOS/Presentation/MyPage/VC/InfoVC/GoalRewardInfoVC.swift
@@ -14,11 +14,24 @@ final class GoalRewardInfoVC: UIViewController {
     // MARK: - UI Components
     
     private lazy var navibar = CustomNavigationBar(self, type: .titleWithLeftButton).setTitle("목표 보상")
-
+    private let stampTopView = UIView()
+    private let stampBottomView = UIView()
+    
+    private let stampImage = UIImageView().then {
+        $0.image = ImageLiterals.imgStamp
+    }
+    
+    private let stampExcourageLabel = UILabel().then {
+        $0.text = "다양한 코스를 달리며 러닝 스탬프를 모아봐요"
+        $0.textColor = .g2
+        $0.font = .b4
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
         setUI()
+        setLayout()
         // Do any additional setup after loading the view.
     }
 }
@@ -40,5 +53,36 @@ extension GoalRewardInfoVC {
     
     private func setUI() {
         view.backgroundColor = .w1
+        stampBottomView.backgroundColor = .m3
+    }
+    
+    private func setLayout() {
+        view.addSubviews(stampTopView, stampBottomView)
+        
+        stampTopView.snp.makeConstraints { make in
+            make.top.equalTo(navibar.snp.bottom)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(235)
+        }
+        
+        stampTopView.addSubviews(stampImage, stampExcourageLabel)
+        
+        stampImage.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.width.equalTo(181)
+            make.height.equalTo(167)
+        }
+        
+        stampExcourageLabel.snp.makeConstraints { make in
+            make.top.equalTo(stampImage.snp.bottom).offset(16)
+            make.centerX.equalToSuperview()
+        }
+        
+        stampBottomView.snp.makeConstraints { make in
+            make.top.equalTo(stampTopView.snp.bottom)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(view.safeAreaLayoutGuide).inset(80)
+        }
     }
 }
