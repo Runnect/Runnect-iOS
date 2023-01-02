2eeb0dcfd1b45c2594d9249df05ea85f80ce6936
Sejin Lee <lsj8706@naver.com>
2023-01-02 14:55:21 +0900

    [Feat] #10 - 닉네임 텍스트 필드 입력 시 UI 처리 구현

--- Runnect-iOS/Runnect-iOS/Presentation/SignIn/VC/SignInVC.swift
+++ Runnect-iOS/Runnect-iOS/Presentation/SignIn/VC/SignInVC.swift
@@ -9,6 +9,10 @@ import UIKit
 
 final class SignInVC: UIViewController {
     
+    // MARK: - Properties
+    
+    private let nicknameMaxLength = 7
+    
     // MARK: - UI Components
     
     private let directionLabel = UILabel().then {
@@ -24,14 +28,17 @@ final class SignInVC: UIViewController {
         $0.clipsToBounds = true
     }
     
-    private let nicknameTextField = UITextField().then {
+    private lazy var nicknameTextField = UITextField().then {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.alignment = .center
         $0.attributedPlaceholder = NSAttributedString(string: "닉네임을 입력해주세요", attributes: [.font: UIFont.h5, .foregroundColor: UIColor.g3, .paragraphStyle: paragraphStyle])
+        $0.font = .h5
+        $0.textColor = .g1
         $0.textAlignment = .center
         $0.layer.cornerRadius = 10
         $0.layer.borderWidth = 1
         $0.layer.borderColor = UIColor.g3.cgColor
+        $0.addTarget(self, action: #selector(textFieldTextDidChange), for: .editingChanged)
     }
     
     private let startButton = CustomButton(title: "시작하기").setEnabled(false)
@@ -42,6 +49,36 @@ final class SignInVC: UIViewController {
         super.viewDidLoad()
         self.setUI()
         self.setLayout()
+        self.setDelegate()
+    }
+}
+
+// MARK: - Methods
+
+extension SignInVC {
+    private func setDelegate() {
+        self.nicknameTextField.delegate = self
+    }
+    
+    private func changeTextFieldLayerColor(_ isEditing: Bool) {
+        nicknameTextField.layer.borderColor = isEditing ? UIColor.m1.cgColor : UIColor.g3.cgColor
+    }
+}
+
+// MARK: - @objc Function
+
+extension SignInVC {
+    @objc private func textFieldTextDidChange() {
+        guard let text = nicknameTextField.text else { return }
+        
+        startButton.isEnabled = !text.isEmpty
+        changeTextFieldLayerColor(!text.isEmpty)
+        
+        if text.count > nicknameMaxLength {
+            let index = text.index(text.startIndex, offsetBy: nicknameMaxLength)
+            let newString = text[text.startIndex..<index]
+            self.nicknameTextField.text = String(newString)
+        }
     }
 }
 
@@ -62,7 +99,7 @@ extension SignInVC {
         
         personImageView.snp.makeConstraints { make in
             make.centerX.equalTo(view.safeAreaLayoutGuide)
-            make.top.equalTo(directionLabel.snp.bottom).offset(93)
+            make.top.equalTo(directionLabel.snp.bottom).offset(93.adjustedH)
         }
         
         nicknameTextField.snp.makeConstraints { make in
@@ -73,8 +110,17 @@ extension SignInVC {
         
         startButton.snp.makeConstraints { make in
             make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(30)
-            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalToSuperview().inset(34)
             make.height.equalTo(44)
         }
+    }
+}
+
+// MARK: - UITextFieldDelegate
+
+extension SignInVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
