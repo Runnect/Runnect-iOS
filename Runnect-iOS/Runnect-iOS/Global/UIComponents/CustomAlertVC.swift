069d2f98166d6381a380271e78a40e22f178cf26
Sejin Lee <lsj8706@naver.com>
2023-01-04 02:07:02 +0900

    [Feat] #27 - 위치 오버레이 아이콘 커스텀

---
+++ Runnect-iOS/Runnect-iOS/Global/UIComponents/CustomAlertVC.swift
@@ -0,0 +1,29 @@
+//
+//  CustomAlertVC.swift
+//  Runnect-iOS
+//
+//  Created by sejin on 2023/01/04.
+//
+
+import UIKit
+
+class CustomAlertVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
