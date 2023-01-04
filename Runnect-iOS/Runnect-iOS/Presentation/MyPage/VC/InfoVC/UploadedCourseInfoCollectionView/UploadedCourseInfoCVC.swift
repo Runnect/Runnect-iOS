6fadc99304411f3a04e211152abc7fec865735b2
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-01-04 12:43:03 +0900

    [Merge] #26 - pull develop

---
+++ Runnect-iOS/Runnect-iOS/Presentation/MyPage/VC/InfoVC/UploadedCourseInfoCollectionView/UploadedCourseInfoCVC.swift
@@ -0,0 +1,14 @@
+//
+//  UploadedCourseInfoCVC.swift
+//  Runnect-iOS
+//
+//  Created by 몽이 누나 on 2023/01/03.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class UploadedCourseInfoCVC: UICollectionViewCell {
+    
+}
--- Runnect-iOS/Runnect-iOS/Presentation/MyPage/VC/InfoVC/UploadedCourseInfoVC.swift
+++ Runnect-iOS/Runnect-iOS/Presentation/MyPage/VC/InfoVC/UploadedCourseInfoVC.swift
@@ -10,9 +10,44 @@ import SnapKit
 import Then
 
 final class UploadedCourseInfoVC: UIViewController {
+    
+    // MARK: - Properties
+    
+    var UploadedCourseList: [UploadedCourseInfoModel] = [
+        UploadedCourseInfoModel(title: "행복한 날들", place: "수원시 장안구"),
+        UploadedCourseInfoModel(title: "몽이랑 산책", place: "수원시 장안구"),
+        UploadedCourseInfoModel(title: "패스트파이브", place: "수원시 장안구"),
+        UploadedCourseInfoModel(title: "행복한 날들", place: "수원시 장안구"),
+        UploadedCourseInfoModel(title: "몽이랑 산책", place: "수원시 장안구"),
+        UploadedCourseInfoModel(title: "패스트파이브", place: "수원시 장안구"),
+        UploadedCourseInfoModel(title: "행복한 날들", place: "수원시 장안구"),
+        UploadedCourseInfoModel(title: "몽이랑 산책", place: "수원시 장안구"),
+        UploadedCourseInfoModel(title: "패스트파이브", place: "수원시 장안구")
+    ]
+    
+    // MARK: - Constants
+    
+    final let uploadedCourseInset: UIEdgeInsets = UIEdgeInsets(top: 16, left: 16, bottom: 0, right: 16)
+    final let uploadedCourseLineSpacing: CGFloat = 20
+    final let uploadedCourseItemSpacing: CGFloat = 10
+    final let uplodaedCourseCellHeight: CGFloat = 124
 
     // MARK: - UI Components
+    
     private lazy var navibar = CustomNavigationBar(self, type: .titleWithLeftButton).setTitle("업로드한 코스")
+    
+    private lazy var UploadedCourseInfoCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isScrollEnabled = true
+        collectionView.showsVerticalScrollIndicator = false
+        //collectionView.delegate = self
+        //collectionView.dataSource = self
+        
+        return collectionView
+    }()
     
     // MARK: - View Life Cycle
     
@@ -20,6 +55,7 @@ final class UploadedCourseInfoVC: UIViewController {
         super.viewDidLoad()
         setNavigationBar()
         setUI()
+        setLayout()
     }
 }
 
@@ -38,5 +74,9 @@ extension UploadedCourseInfoVC {
     
     private func setUI() {
         view.backgroundColor = .w1
+    }
+    
+    private setLayout() {
+        
     }
 }
