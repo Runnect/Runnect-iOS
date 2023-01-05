5d03d3c099325af9346391692c13b88c91401d2a
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-01-05 15:47:49 +0900

    [Feat] #41 - CourseDetailVC 파일 생성

---
+++ Runnect-iOS/Runnect-iOS/Presentation/CourseDetail/VC/CourseDetailVC.swift
@@ -0,0 +1,27 @@
+//
+//  CourseDetailVC.swift
+//  Runnect-iOS
+//
+//  Created by 몽이 누나 on 2023/01/05.
+//
+
+import UIKit
+
+final class CourseDetailVC: UIViewController {
+    
+    // MARK: - UI Components
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension CourseDetailVC {
+    
+    // MARK: - Layout Helpers
+    
+    
+    
+}
