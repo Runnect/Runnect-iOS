67a1a8ddf2dc6b842dc71a2efd7dedb0d83dbac1
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-01-05 15:56:14 +0900

    [Feat] #41 - NavigationBar 생성

--- Runnect-iOS/Runnect-iOS/Presentation/CourseDetail/VC/CourseDetailVC.swift
+++ Runnect-iOS/Runnect-iOS/Presentation/CourseDetail/VC/CourseDetailVC.swift
@@ -6,22 +6,41 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 final class CourseDetailVC: UIViewController {
     
     // MARK: - UI Components
+    private lazy var navibar = CustomNavigationBar(self, type: .titleWithLeftButton)
 
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationBar()
+        setUI()
+        setLayout()
     }
 }
 
 extension CourseDetailVC {
     
     // MARK: - Layout Helpers
+    private func setNavigationBar() {
+        view.addSubview(navibar)
+        
+        navibar.snp.makeConstraints {  make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(48)
+        }
+    }
     
+    private func setUI() {
+        
+    }
     
-    
+    private func setLayout() {
+        
+    }
 }
