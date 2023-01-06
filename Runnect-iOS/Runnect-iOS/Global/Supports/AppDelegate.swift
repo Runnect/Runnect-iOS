46e2ae73ac31e40e2381ab9ec15ab250536d5bf4
Sejin Lee <lsj8706@naver.com>
2023-01-06 22:59:44 +0900

    [Chore] #52 - 지도 ClientId info.plist에서 Config 파일로 이동

--- Runnect-iOS/Runnect-iOS/Global/Supports/AppDelegate.swift
+++ Runnect-iOS/Runnect-iOS/Global/Supports/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import NMapsMap
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,7 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        NMFAuthManager.shared().clientId = Config.naverMapClientId
+        
         return true
     }
 
