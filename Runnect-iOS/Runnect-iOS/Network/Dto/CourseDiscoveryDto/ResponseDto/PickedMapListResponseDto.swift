0aeb849bc43b9973de83ae843111706f970870f7
yeonwoo <y8551472@gmail.com>
2023-01-10 19:06:07 +0900

    [Fix] #67 - 코멘트 수정

--- Runnect-iOS/Runnect-iOS/Network/Dto/CourseDiscoveryDto/ResponseDto/PickedMapListResponseDto.swift
+++ Runnect-iOS/Runnect-iOS/Network/Dto/CourseDiscoveryDto/ResponseDto/PickedMapListResponseDto.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-// MARK: - DataClass
+// MARK: - PickedMapListResponseDto
 
 struct PickedMapListResponseDto: Codable {
     let publicCourses: [PublicCourse]
@@ -23,7 +23,7 @@ struct PublicCourse: Codable {
     let departure: CourseDiscoveryDeparture
 }
 
-// MARK: - Departure
+// MARK: - CourseDiscoveryDeparture
 
 struct CourseDiscoveryDeparture: Codable {
     let region: String
