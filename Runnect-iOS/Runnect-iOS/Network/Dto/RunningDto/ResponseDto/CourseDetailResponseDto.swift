4216078a8d2258bb9c7a4a20e7c43c814018a1f5
Sejin Lee <lsj8706@naver.com>
2023-01-10 23:56:50 +0900

    [Feat] #74 - 코스 디테일을 받기 위한 Dto및 라우터 구현

---
+++ Runnect-iOS/Runnect-iOS/Network/Dto/RunningDto/ResponseDto/CourseDetailResponseDto.swift
@@ -0,0 +1,17 @@
+//
+//  CourseDetailResponseDto.swift
+//  Runnect-iOS
+//
+//  Created by sejin on 2023/01/10.
+//
+
+import Foundation
+
+typealias Course = PrivateCourse
+typealias CourseDeparture = PrivateCourseDeparture
+
+// MARK: - CourseDetailResponseDto
+
+struct CourseDetailResponseDto: Codable {
+    let course: [Course]
+}
