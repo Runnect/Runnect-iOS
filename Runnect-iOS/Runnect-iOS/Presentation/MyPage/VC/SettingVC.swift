30bc29ca917f4af39c1381bb3f3889912b2cfa94
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-04-10 01:09:56 +0900

    [Feat] #127 - MyPageVC 수정된 UI components 구현

---
+++ Runnect-iOS/Runnect-iOS/Presentation/MyPage/VC/SettingVC.swift
@@ -0,0 +1,35 @@
+//
+//  SettingVC.swift
+//  Runnect-iOS
+//
+//  Created by 몽이 누나 on 2023/04/10.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SettingVC: UIViewController {
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+    
+}
+
+// MARK: - UI & Layout
+
+extension SettingVC {
+    private func setUI() {
+        view.backgroundColor = .w1
+    }
+    
+    private func setLayout() {
+        
+    }
+}
