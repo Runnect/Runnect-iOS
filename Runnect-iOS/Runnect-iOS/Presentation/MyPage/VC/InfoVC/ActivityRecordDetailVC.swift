870a07035bcf3c883bd00c0f3a2f2166bb7212b2
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-05-11 01:08:13 +0900

    [Feat] #136 - 선택된 코스 setData

--- Runnect-iOS/Runnect-iOS/Presentation/MyPage/VC/InfoVC/ActivityRecordDetailVC.swift
+++ Runnect-iOS/Runnect-iOS/Presentation/MyPage/VC/InfoVC/ActivityRecordDetailVC.swift
@@ -41,9 +41,9 @@ final class ActivityRecordDetailVC: UIViewController {
         $0.font = .h4
     }
     
-    private let recordDateInfoView = CourseDetailInfoView(title: "날짜", description: "0000.00.00")
+    private let recordDateInfoView = CourseDetailInfoView(title: "날짜", description: String())
     
-    private let recordDepartureInfoView = CourseDetailInfoView(title: "출발지", description: "서울시 영등포구")
+    private let recordDepartureInfoView = CourseDetailInfoView(title: "출발지", description: String())
     
     private lazy var recordInfoStackView = UIStackView(arrangedSubviews: [recordDateInfoView, recordDepartureInfoView]).then {
         $0.axis = .vertical
@@ -115,19 +115,50 @@ extension ActivityRecordDetailVC {
     func setData(model: ActivityRecord) {
         self.mapImageView.setImage(with: model.image)
         self.courseTitleLabel.text = model.title
-        self.recordDistanceValueLabel.text = "\(model.distance) km"
-        self.recordRunningTimeValueLabel.text = model.time
-        self.recordAveragePaceValueLabel.text = model.pace
-        self.recordDateInfoView.setDescriptionText(description: model.createdAt)
-        self.recordDepartureInfoView.setDescriptionText(description: model.departure.region + " " + model.departure.city)
+        
+        let location = "\(model.departure.region) \(model.departure.city)"
+        self.recordDepartureInfoView.setDescriptionText(description: location)
+        
+        // 날짜 바꾸기
+        let recordDate = model.createdAt.prefix(10)
+        let resultDate = RNTimeFormatter.changeDateSplit(date: String(recordDate))
+        self.recordDateInfoView.setDescriptionText(description: resultDate)
+        
+        // 이동 시간 바꾸기
+        let recordRunningTime = model.time.suffix(7)
+        self.recordRunningTimeValueLabel.text = String(recordRunningTime)
+        
+        // 평균 페이스 바꾸기
+        let array = spiltRecordAveragePace(model: model)
+        setUpRecordAveragePaceValueLabel(array: array, label: recordAveragePaceValueLabel)
+        setUpRecordDistanceValueLabel(model: model, label: recordDistanceValueLabel)
     }
     
     func setDetailInfoStakcView(title: UIView, value: UIView) -> UIStackView {
         let stackView = UIStackView(arrangedSubviews: [title, value])
         stackView.axis = .vertical
         stackView.alignment = .center
-        stackView.spacing = 2
+        stackView.spacing = 8
         return stackView
+    }
+    
+    private func spiltRecordAveragePace(model: ActivityRecord) -> [String] {
+        let recordAveragePace = model.pace
+        let array = recordAveragePace.split(separator: ":").map { String($0) }
+        return array
+    }
+    
+    private func setUpRecordAveragePaceValueLabel(array: [String], label: UILabel) {
+        let numberArray = array.compactMap { Int($0) }   // 페이스에서 첫번째 인덱스 두번째 값만 가져오기 위해
+        let attributedString = NSMutableAttributedString(string: String(numberArray[1]) + "’", attributes: [.font: UIFont.h3, .foregroundColor: UIColor.g1])
+        attributedString.append(NSAttributedString(string: String(array[2]) + "”", attributes: [.font: UIFont.h3, .foregroundColor: UIColor.g1]))
+        label.attributedText = attributedString
+    }
+    
+    private func setUpRecordDistanceValueLabel(model: ActivityRecord, label: UILabel) {
+        let attributedString = NSMutableAttributedString(string: String(model.distance) + " ", attributes: [.font: UIFont.h3, .foregroundColor: UIColor.g1])
+        attributedString.append(NSAttributedString(string: "km", attributes: [.font: UIFont.b4, .foregroundColor: UIColor.g2]))
+        label.attributedText = attributedString
     }
     
     func setBlackTitle() -> UILabel {
@@ -246,38 +277,6 @@ extension ActivityRecordDetailVC {
             make.top.equalTo(secondHorizontalDivideLine.snp.bottom).offset(23)
             make.centerX.equalToSuperview()
             make.bottom.equalToSuperview().inset(30)
-        }
-    }
-}
-
-// MARK: - Network
-
-extension ActivityRecordDetailVC {
-    func getActivityRecordDetailWithPath() {
-        LoadingIndicator.showLoading()
-        recordProvider.request(.getActivityRecordInfo) { [weak self] response in
-            LoadingIndicator.hideLoading()
-            guard let self = self else { return }
-            switch response {
-            case .success(let result):
-                let status = result.statusCode
-                if 200..<300 ~= status {
-                    do {
-                        let responseDto = try result.map(BaseResponse<ActivityRecordInfoDto>.self)
-                        guard let data = responseDto.data else { return }
-                        //self.courseModel = data.course
-                    } catch {
-                        print(error.localizedDescription)
-                    }
-                }
-                if status >= 400 {
-                    print("400 error")
-                    self.showNetworkFailureToast()
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-                self.showNetworkFailureToast()
-            }
         }
     }
 }
