1a805000382a616b5eb4e81b3cd50f9487442883
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-05-10 14:36:44 +0900

    [Feat] #130 - ActivityRecordDetailVC 파일 생성

---
+++ Runnect-iOS/Runnect-iOS/Presentation/MyPage/VC/InfoVC/ActivityRecordDetailVC.swift
@@ -0,0 +1,34 @@
+//
+//  ActivityRecordDetailVC.swift
+//  Runnect-iOS
+//
+//  Created by 몽이 누나 on 2023/05/10.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ActivityRecordDetailVC: UIViewController {
+
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+}
+
+// MARK: - Layout Helpers
+
+extension ActivityRecordDetailVC {
+    private func setUI() {
+        view.backgroundColor = .w1
+    }
+    
+    private func setLayout() {
+        
+    }
+}
