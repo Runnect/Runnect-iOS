bedc514f219d541d21f4d19d4d3191855fd7ff69
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-10-06 22:15:19 +0900

    [Feat] #192 - DropDown 파일생성
    라이브러리 다운

---
+++ Runnect-iOS/Runnect-iOS/Global/UIComponents/RNMenuVC.swift
@@ -0,0 +1,29 @@
+//
+//  RNMenuVC.swift
+//  Runnect-iOS
+//
+//  Created by 이명진 on 2023/10/06.
+//
+
+import UIKit
+
+class RNMenuVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
