db7e87e8fcb4e2bdabac45df663d747b364a2a00
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-18 17:55:34 +0900

    [Feat] #194 - 마라톤 Cell skeleton 코드 추가

---
+++ Runnect-iOS/Runnect-iOS/Presentation/CourseDiscovery/Views/MarathonMapCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MarathonMapCollectionViewCell.swift
+//  Runnect-iOS
+//
+//  Created by 이명진 on 11/18/23.
+//
+
+import UIKit
+
+class MarathonMapCollectionViewCell: UICollectionViewCell {
+    
+}
