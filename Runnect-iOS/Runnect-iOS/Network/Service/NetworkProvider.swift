a6b57ca49533ec5c27c4449ecf6cad8cf6c56678
SojinLee <513sojin@naver.com>
2023-11-21 17:57:45 +0900

    [Feat] #207 - 에러메세지 출력 코드 추가

--- Runnect-iOS/Runnect-iOS/Network/Service/NetworkProvider.swift
+++ Runnect-iOS/Runnect-iOS/Network/Service/NetworkProvider.swift
@@ -15,6 +15,7 @@ class NetworkProvider<Provider : TargetType> : MoyaProvider<Provider> {
     func request<Model : Codable>(target : Provider, instance : Model.Type , vc: UIViewController, completion : @escaping(ResponseResult<Model>) -> ()){
         self.request(target) { result in
             switch result {
+            /// 서버 통신 성공
             case .success(let response):
                 if (200..<300).contains(response.statusCode) {
                     if let decodeData = try? JSONDecoder().decode(instance, from: response.data) {
@@ -29,7 +30,15 @@ class NetworkProvider<Provider : TargetType> : MoyaProvider<Provider> {
                 }
             /// 서버 통신 실패
             case .failure(let error):
-                print(error.errorDescription)
+                if let response = error.response {
+                    if let responseData = String(data: response.data, encoding: .utf8) {
+                        print(responseData)
+                    } else {
+                        print(error.localizedDescription)
+                    }
+                } else {
+                    print(error.localizedDescription)
+                }
                 vc.showNetworkFailureToast()
             }
         }
