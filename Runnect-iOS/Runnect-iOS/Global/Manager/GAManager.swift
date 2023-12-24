f570bd9466061c791c717e97b24d0b4cdb36f6ba
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-12-24 21:28:59 +0900

    [Feat] #230 - GA 코드 추가

---
+++ Runnect-iOS/Runnect-iOS/Global/Manager/GAManager.swift
@@ -0,0 +1,39 @@
+//
+//  GAManager.swift
+//  Runnect-iOS
+//
+//  Created by 이명진 on 12/24/23.
+//
+
+import UIKit
+
+import FirebaseAnalytics
+
+final class GAManager {
+    static let shared = GAManager()
+    
+    private init() {}
+    
+    enum EventType {
+        case screen(screenName: String)
+        
+        var eventName: String {
+            switch self {
+            case .screen:
+                return "screen"
+            }
+        }
+        
+        var parameters: [String: Any]? {
+            switch self {
+            case .screen(let screenName):
+                ["screen": screenName]
+            }
+        }
+    }
+    
+    func logEvent(eventType: EventType) {
+        Analytics.logEvent(eventType.eventName, parameters: eventType.parameters)
+    }
+    
+}
