3122ab20c09b9ad94c53c02962a9f766346e122d
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-01-03 20:03:57 +0900

    [Add] #230 - GA 이벤트 추가
    보관함, 마이페이지, 방문자 모드에서 발생할 수 있는 이벤트를 추가하였습니다.

--- Runnect-iOS/Runnect-iOS/Global/Analytics/Event.swift
+++ Runnect-iOS/Runnect-iOS/Global/Analytics/Event.swift
@@ -15,36 +15,27 @@ struct Event {
     struct Button {
         
         // 로그인
-        
         static let clickAppleLogin = "click_apple_login"
         static let clickKaKaoLogin = "click_kakao_login"
         
-        
         // 탭바
-        
         static let clickCourseDrawingTabBar = "click_course_drawing_tab_bar"
         static let clickStorageTabBar = "click_storage_tab_bar"
         static let clickCourseDiscoveryTabBar = "click_course_discovery_tab_bar"
         static let clickMyPageTabBar = "click_my_page_tab_bar"
         
-        
         // 코스 그리기
-        
         static let clickCourseDrawing = "click_course_drawing"
         static let clickCurrentLocate = "click_current_locate"
         static let clickMapLocate = "click_map_locate"
         static let clickStoredAfterCourseComplete = "click_stored_after_course_complete"
         static let clickRunAfterCourseComplete = "click_run_after_course_complete"
         
-        
         // 러닝 트래킹
-        
         static let clickBackRunningTracking = "click_back_running_tracking"
         static let clickStoreRunningTracking = "click_store_running_tracking"
         
-        
         /// 코스 발견
-        
         static let clickDate = "click_date_sort"
         static let clickScrap = "click_scrap_sort"
         static let clickUploadButton = "click_upload_button"
@@ -57,7 +48,27 @@ struct Event {
         static let clickScrapPageStartCourse = "click_scrap_page_start_course"
         static let clickUploadCourse = "click_upload_course"
 
-        
         /// 보관함
+        static let clickMyStorageCourseStart = "click_my_storage_course_start"
+        static let clickMyStorageTryModify = "click_my_storage_try_modify"
+        static let clickMyStorageTryRemove = "click_my_storage_try_remove"
+        static let clickScrapCourse = "click_scrap_course"
+        
+        // 마이페이지
+        static let clickRunningRecord = "click_running_record"
+        static let clickGoalReward = "click_goal_reward"
+        static let clickUploadedCourse = "click_uploaded_course"
+        static let clickCourseDrawingInRunningRecord = "click_course_drawing_in_running_record"
+        static let clickCourseUploadInUploadedCourse = "click_course_upload_in_uploaded_course"
+        static let clickTryLogout = "click_try_logout"
+        static let clickTryWithdraw = "click_try_withdraw"
+        static let clickSuccessLogout = "click_success_logout"
+        static let clickSuccessWithdraw = "click_success_withdraw"
+        
+        /// 방문자 모드
+        static let clickJoinInCourseDrawing = "click_join_in_course_drawing"
+        static let clickJoinInCourseDiscovery = "click_join_in_course_discovery"
+        static let clickJoinInStorage = "click_join_in_storage"
+        static let clickJoinInMyPage = "click_join_in_my_page"
     }
 }
