725a3095b579208b9038909658e0180b2c47f01f
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-02-07 18:40:32 +0900

    [Feat] #253 - Remote Config 구현

--- Runnect-iOS/Runnect-iOS/Presentation/Splash/VC/SplashVC.swift
+++ Runnect-iOS/Runnect-iOS/Presentation/Splash/VC/SplashVC.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import Combine
 
 import SnapKit
 import Then
+import FirebaseRemoteConfig
 
 final class SplashVC: UIViewController {
+    
+    // MARK: - Property
+    private var cancelBag = CancelBag()
     
     // MARK: - UI Components
     
@@ -31,7 +36,14 @@ final class SplashVC: UIViewController {
         self.setUI()
         self.setNavigationBar()
         self.setLayout()
-        self.checkDidSignIn()
+        
+        self.setRemoteConfig()
+        
+        NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)
+            .sink { [weak self] _ in
+                self?.setRemoteConfig()
+            }
+            .store(in: cancelBag)
     }
 }
 
@@ -89,6 +101,75 @@ extension SplashVC {
         
         logoImageView.snp.makeConstraints { make in
             make.center.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+extension SplashVC {
+    func showUpdateAlert() {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(
+                title: "업데이트 알림",
+                message: "새로운 기능이 추가된 'Runnect'의 최신 버전을 만나보세요!\n지금 바로 업데이트하고 개선된 사용자 경험을 즐겨보세요.",
+                preferredStyle: .alert
+            )
+            
+            let updateAction = UIAlertAction(title: "업데이트 링크", style: .default) { [self] _ in
+                openAppstore()
+            }
+            
+            alert.addAction(updateAction)
+            
+            self.present(alert, animated: false)
+            
+        }
+    }
+    
+    private func openAppstore() {
+        guard let url = URL(string: "itms-apps://itunes.apple.com/app/1663884202") else { return }
+        
+        if UIApplication.shared.canOpenURL(url) {
+            if #available(iOS 13.0, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
+    
+    /// ** RemoteConfig  **
+    /// 심사버전 > 스토어 버전이면 강제 업데이트 Alert을 진행 합니다.
+    private func setRemoteConfig() {
+        let remoteConfig = RemoteConfig.remoteConfig()
+        let settings = RemoteConfigSettings()
+        
+        settings.minimumFetchInterval = 0 // 개발 중에는 0으로 설정, 실제 앱에서는 적절한 값을 설정
+        remoteConfig.configSettings = settings
+        
+        remoteConfig.fetch { (status, error) -> Void in
+            if status == .success {
+                remoteConfig.activate { (changed, error) in
+                    // 현재 앱 버전 가져오기
+                    guard let info = Bundle.main.infoDictionary,
+                          let currentVersion = info["CFBundleShortVersionString"] as? String,
+                          let _ = info["CFBundleIdentifier"] as? String,
+                          let storeVersion = remoteConfig["iOS_current_market_version"].stringValue
+                    else { return }
+                    
+                    print("‼️‼️‼️‼️\(currentVersion), ‼️‼️‼️‼️ \(storeVersion)")
+                    // 스토어 버전과 현재 버전 비교
+                    if currentVersion.compare(storeVersion, options: .numeric) == .orderedAscending {
+                        // 스토어 버전이 더 높으면 업데이트 필요
+                        self.showUpdateAlert()
+                    } else {
+                        // 업데이트가 필요하지 않으면 기본 로직 수행
+                        self.checkDidSignIn()
+                    }
+                }
+            } else {
+                print("Error fetching remote config: \(error?.localizedDescription ?? "No error available.")")
+                self.checkDidSignIn()
+            }
         }
     }
 }
